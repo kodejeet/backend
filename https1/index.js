817127202c8e09588fec64b4fff189d5ebbccc94
kodejeet <roshansahx2003@gmail.com>
2025-03-10 02:38:21 +0530

    no url

--- https1/index.js
+++ https1/index.js
@@ -1,12 +1,21 @@
 // https server without express js 
 
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) =>{
     //console.log("New Req Received");    ----1
     // console.log(req.headers);          ----2
-    console.log(req);
-    res.end("Hello From Server");
+    // console.log(req);                  ----3
+
+     const log = `${Date.now()}:  New Request Received\n`;   
+    fs.appendFile("log.txt", log, (err,data) => {
+        res.end("Hello From Server");
+    });     // non-blocking req
+    
+    // res.end("Hello From Server");
 });
 
 myServer.listen(8000, ()=> console.log("Server Started"));
+
+//  we can send the whole html too thats called server side rendering
