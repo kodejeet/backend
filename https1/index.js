42c0e4f0499fa5fb03819a355ad9649ba2b9539d
kodejeet <roshansahx2003@gmail.com>
2025-03-10 01:50:46 +0530

    basic done

--- https1/index.js
+++ https1/index.js
@@ -1,3 +1,10 @@
 // https server without express js 
 
-const https = 
+const http = require("http");
+
+const myServer = http.createServer((req, res) =>{
+    console.log("New Request Received");
+    res.end("Hello From Server");
+});
+
+myServer.listen(8000, ()=> console.log("Server Started"));
