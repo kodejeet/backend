e20cbf1e7a759f3c8b6d7880568e32d27ea6b816
kodejeet <roshansahx2003@gmail.com>
2025-03-10 02:01:21 +0530

    req

--- https1/index.js
+++ https1/index.js
@@ -3,8 +3,9 @@
 const http = require("http");
 
 const myServer = http.createServer((req, res) =>{
-    //console.log("New Req Received");
-    console.log(req.headers);
+    //console.log("New Req Received");    ----1
+    // console.log(req.headers);          ----2
+    console.log(req);
     res.end("Hello From Server");
 });
 
