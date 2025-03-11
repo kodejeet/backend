8264d5e7bef8169e2fcc2528233e729fb0f2fdc0
kodejeet <roshansahx2003@gmail.com>
2025-03-11 23:17:39 +0530

    url handling

--- https-server/https1/index.js
+++ https-server/https1/index.js
@@ -2,8 +2,14 @@
 
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const myServer = http.createServer((req, res) =>{
+    if(req.url==='/favicon.ico') return res.end();
+
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl); 
+
     //console.log("New Req Received");    ----1
     // console.log(req.headers);          ----2
     // console.log(req);                  ----3
@@ -12,11 +18,21 @@ const myServer = http.createServer((req, res) =>{
     fs.appendFile("log.txt", log, (err,data) => {
         // res.end("Hello From Server");
         // multi route using switch case 
-        switch(req.url){
-            case '/': res.end("HomePage");
-            break
-            case '/about': res.end("I am Anon404");
+        switch(myUrl.pathname){
+            case '/': 
+            res.end("HomePage");
             break;
+            case '/about':
+                // res.end("I am Anon404");
+            const username = myUrl.query.myname;
+            res.end(`Hi, ${username}`);
+            break;
+
+            case '/search':
+                const search = myUrl.query.search_query;
+                res.end('Here are your results for :' +search);
+                break;
+
             default:
                 res.end("404 Not Found")
         }
