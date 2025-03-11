a6ad1afe2d445bb4c99379f709f5c3d03d51bee8
kodejeet <roshansahx2003@gmail.com>
2025-03-11 15:45:11 +0530

    different approach

--- middlewares-authentication/script.js
+++ middlewares-authentication/script.js
@@ -5,11 +5,23 @@ const app = express();
 app.get("/health-checkup", function(req, res){
     const username = req.headers.username;
     const password = req.headers.password;
+    const kidneyId = req.query.kidneyKid;
 
-    if (username === "anon404" && password ==="pass"){
-        // do something with kidney here 
-        res.json({
-            msg: "Your Kindney is fine."
-        })
+    if (username != "anon404" || password !="pass"){
+        res.status(400).json({"msg" : "Something up with your inputs"})
+        return
     }
+    if(kidneyId != 1 && kidneyId != 2){
+        res.status(400).json({"msg" : "Something up with your inputs"})
+        return
+    }
+    // do something with kidney here
+    
+    res.json({
+        msg: "Your Kidney is fine."
+    })
+        
+    // res.status(400).json({"msg" : "Something up with your inputs"})
 });
+
+app.listen(3000);
