d532088d1b70dc6deadb497240eac58413074402
kodejeet <roshansahx2003@gmail.com>
2025-03-15 06:44:08 +0530

    tested diff methods with sample json data

--- basics/04/index.js
+++ basics/04/index.js
@@ -1,54 +1,59 @@
-// building a REST API from scratch 
+// building a REST API from scratch
 
 const express = require("express");
-const users = require('./MOCK_DATA.json');
+const users = require("./MOCK_DATA.json");
 const app = express();
 const PORT = 1111;
 
+const fs = require("fs");
+
+// middleware plugin
+app.use(express.urlencoded({ extended: false }));
+
 // routes
 
-
 // for web-view
-app.get('/api/users', function (req, res) {
-        return res.json(users);
-    });
+app.get("/api/users", function (req, res) {
+  return res.json(users);
+});
 
 app.get("/users", (req, res) => {
-        const html = `
+  const html = `
         <ul>
             ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
         </ul>
         `;
-        res.send(html);
-    });
+  res.send(html);
+});
 
-// access through dynamic path 
+// access through dynamic path
 
-// efficient way of multiple https method routing 
+// efficient way of multiple https method routing
 
 app
-    .route("/api/users/:id")
-    .get((req,  res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
-        return res.json(user);
-    })
-    .patch((req, res) => {
-        // edit user with id 
-        res.json({status: "pending"});
-    })
-    .delete((req, res) => {
-        // delete user with id 
-        res.json({status: "pending"});
-    })
-    // .patch((req, res) => {})
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+  })
+  .patch((req, res) => {
+    // edit user with id
+    res.json({ status: "pending" });
+  })
+  .delete((req, res) => {
+    // delete user with id
+    res.json({ status: "pending" });
+  });
+// .patch((req, res) => {})
 
-
-
-
-app.post("/api/users", (req,  res) => {
-    // create new user 
-    return res.json({status: "pending"});
+app.post("/api/users", (req, res) => {
+  // create new user
+  const body = req.body;
+  users.push({ ...body, id: users.length + 1 });
+  fs.writeFile("././MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "success", id: users.length});
+  });
 });
 
 app.listen(PORT, () => console.log(`Server Started at PORT: ${PORT}`));
