9b6ff25ac9eae515d7aea3bab3fb41001cb6e4e0
kodejeet <roshansahx2003@gmail.com>
2025-03-18 05:47:51 +0530

    delete patch

--- basics/05/index.js
+++ basics/05/index.js
@@ -28,6 +28,44 @@ const User = mongoose.model("user", userSchema);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// get route 
+
+app.get("/users", async (req, res) => {
+  const allDbUsers = await User.find({});
+  const html = `
+        <ul>
+            ${allDbUsers
+              .map((user) => `<li>${user.firstName} - ${user.email}</li>`)
+              .join("")}
+        </ul>
+        `;
+  res.send(html);
+});
+
+app.get("/api/users", async (req, res) => {
+    // get single user by id 
+    const allDbUsers = await User.find({});
+    return res.json(allDbUsers);
+  });
+
+  app.route("/api/users/:id")
+    .get(async (req, res) => {
+      // get single user by id 
+      const user = await User.findById(req.params.id);
+      if (!user) return res.status(404).json({error: "user not found"});
+      return res.json(user);
+    })
+    .patch(async (req, res) => {
+        // edit user with id
+        await User.findByIdAndUpdate(req.params.id, {lastName: "Changed"});
+        return res.json({status: "Success"});
+      })
+    .delete(async (req, res) => {
+        // delete user with id
+        await User.findByIdAndDelete(req.params.id);
+        return res.json({status: "Success"});
+      });
+
 // POST route
 app.post("/api/users", async (req, res) => {
   const { first_name, last_name, email, gender, job_title } = req.body;
